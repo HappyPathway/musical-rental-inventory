460b3763094eff289f2aa9b73834f52b6973299f
Dave Arnold <dave@roknsound.com>
2025-04-03 00:19:56 -0700

    fix: Update moved configuration to use specific state bucket reference

--- backend-state/moved.tf
+++ backend-state/moved.tf
@@ -1,4 +1,4 @@
 moved {
   from = google_storage_bucket.terraform_state
-  to = google_storage_bucket.terraform_state[var.state_bucket_name] 
+  to = google_storage_bucket.terraform_state["roknsound-tfstate"] 
 }
