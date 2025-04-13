cf4c3131db6ce840a542e25c4cd53100128689a2
Dave Arnold <dave@roknsound.com>
2025-04-12 23:41:16 -0700

    Remove infrastructure and GitHub configuration files for the musical rental inventory project. This includes the deletion of Terraform files related to Google Cloud resources, IAM roles, Cloud Run services, Cloud SQL instances, and GitHub repository management. Additionally, workspace files for both the main project and Django application have been removed.

--- backend-state/moved.tf
+++
@@ -1,4 +0,0 @@
-moved {
-  from = google_storage_bucket.terraform_state
-  to = google_storage_bucket.terraform_state["roknsound-tfstate"] 
-}
--- infra/main.tf
+++
@@ -1,8 +0,0 @@
-# Main Terraform configuration file
-# Contains shared resources and those that don't fit elsewhere
-
-# Generate a random password for Django secret key
-resource "random_password" "django_secret" {
-  length  = 32
-  special = true
-}
